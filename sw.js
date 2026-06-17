299ed1b1ff645688c045162c785c70c9017a30f7
pazarman <pazarman@gmail.com>
2026-06-16 23:07:11 -0700

    Fix Body Match viewport clipping: Optimized zoom-face scale and translation; Bumped SW cache v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE whenever you ship an update (forces old caches to clear).
-const CACHE = "little-explorer-v14";
+const CACHE = "little-explorer-v15";
 const ASSETS = [
   "./", "./index.html", "./manifest.json", "./icon.svg",
   "./css/style.css",
